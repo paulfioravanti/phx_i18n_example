1e3c72cab23b190302616721845650a0dab19be8
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-10-20 17:05:34 +1100

    Add Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,21 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phx_i18n_example, PhxI18nExampleWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [
+    host: "phx-i18n-03-js-takeover.herokuapp.com",
+    port: 443,
+    scheme: "https"
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :phx_i18n_example, PhxI18nExample.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -52,4 +65,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
