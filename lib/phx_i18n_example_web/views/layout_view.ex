e873fc77aa6315533966712e83ee41038e9d3c83
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-10-20 21:05:18 +1100

    Minor refactor

--- lib/phx_i18n_example_web/views/layout_view.ex
+++ lib/phx_i18n_example_web/views/layout_view.ex
@@ -1,6 +1,6 @@
 defmodule PhxI18nExampleWeb.LayoutView do
   use PhxI18nExampleWeb, :view
-  alias PhxI18nExampleWeb.LayoutStyle
+  alias PhxI18nExampleWeb.{LanguageDropdownView, LayoutStyle}
 
   defdelegate body, to: LayoutStyle
 end
