d753ef7e3cd46f462b16959f602538fc066fa6d4
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-14 15:32:41 +1100

    Expose live_component helper functions

--- lib/phx_i18n_example_web.ex
+++ lib/phx_i18n_example_web.ex
@@ -49,7 +49,10 @@ defmodule PhxI18nExampleWeb do
           live_render: 2,
           live_render: 3,
           live_link: 1,
-          live_link: 2
+          live_link: 2,
+          live_component: 2,
+          live_component: 3,
+          live_component: 4
         ]
 
       alias PhxI18nExampleWeb.Router.Helpers, as: Routes
