715804840d056fdd34944362ac09d2fd1bd67797
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-01-28 15:37:13 +1100

    Update mix dependencies

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule PhxI18nExample.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, "~> 0.4.0"},
+      {:phoenix_live_view, "~> 0.6.0"},
       {:floki, ">= 0.0.0", only: :test}
     ]
   end
