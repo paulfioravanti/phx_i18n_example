bde51ccaa2afbd42e45afa078bd762b64d37d2fe
Paul Fioravanti <paul.fioravanti@gmail.com>
2020-03-01 21:45:09 +1100

    Update to LiveView 0.8.1

--- mix.exs
+++ mix.exs
@@ -34,12 +34,12 @@ defmodule PhxI18nExample.MixProject do
     [
       {:phoenix, "~> 1.4.12"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 2.14"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, "~> 0.6.0"},
+      {:phoenix_live_view, "~> 0.8.0"},
       {:floki, ">= 0.0.0", only: :test}
     ]
   end
