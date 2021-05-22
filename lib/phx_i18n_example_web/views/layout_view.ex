f901eaf2d78916e92797b404fa726e4cb0d92476
Paul Fioravanti <paul.fioravanti@gmail.com>
2021-05-22 22:16:22 +1000

    Wrap functions around Routes.static_path

--- lib/phx_i18n_example_web/views/layout_view.ex
+++ lib/phx_i18n_example_web/views/layout_view.ex
@@ -3,4 +3,7 @@ defmodule PhxI18nExampleWeb.LayoutView do
   alias PhxI18nExampleWeb.{LanguageDropdownLiveComponent, LayoutStyle}
 
   defdelegate body, to: LayoutStyle
+
+  def stylesheet_href(conn), do: Routes.static_path(conn, "/css/app.css")
+  def js_src(conn), do: Routes.static_path(conn, "/js/app.js")
 end
